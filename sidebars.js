f29a996703fc54ff1ee9ecd7276e7a539f276e81
Piotr Chomiak <87984472+chomipi88@users.noreply.github.com>
2021-10-25 11:30:47 -0400

    add new doc link

--- sidebars.js
+++ sidebars.js
@@ -66,7 +66,8 @@ module.exports = {
       },
       'known-limitations',
       'resource-reference-aws',
-      'global-variables'
+      'global-variables',
+      'update-iam-role-trust-relationship'
       ],
     },
   ],
