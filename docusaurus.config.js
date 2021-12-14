8d18e44ca864ba6a8a54822fd93ff29e83ecde69
Jacob Witz <>
2021-12-14 16:37:39 +0000

    Fix docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -186,7 +186,7 @@ module.exports = {
         lastVersion: 'current',
         versions: {
         current: {
-          label: '0.26',
+          label: '0.27',
           path: '',
           banner: 'none',
          },
